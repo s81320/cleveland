27261dc2d9771ccb4199b682958bd91842a6bc24
silke <s81320@beuth-hochschule.de>
2021-06-22 11:06:03 +0200

    (affine) linear exec times (in number of trees)

--- code/basecases/speed/readme.md
+++ code/basecases/speed/readme.md
@@ -2,15 +2,9 @@
 
 Getting predictions for a random forest of 500 trees should take longer than getting predictions for a random forest of 50 trees. But how much longer exactly?
 
-We should get a graph for a full forest of 500 trees and then halve the number of trees to 250 , 125, 62, 31
-
 I use the microbenchmark package suggested in http://adv-r.had.co.nz/Profiling.html
 
-Of course to speed up prediction time we would do predictions for the individual trees in parallel and then collect all results for the majority vote. 
+Of course to speed up prediction time we would do predictions for the individual trees in parallel and then collect all results for the majority vote. This is available in ranger when setting num.threads in ranger::predict. The effect of reducing the number of trees in each thread should be similar (though not the same) to the effect when working in a sequential mode only (only the individual predictions are parallized, the vote has to run on the combined predictions).
 
 ## first result, sequential execution
-On my laptop reducing the size of the forest from 500 to 60 (500/8=62.5, halving it 3 times) the execution time is reduced to about 40% , 45% , a tweak better than halved. So, roughly: Halving the number of trees 3 times results in halving the execution time (once).
-
-Another calculation can be summarised as: Reducing the number of trees to 10% did a little better than halving the execution time (46%).
-
-(This is ok, since 50 is about equal to 60 and reducing a forest by 10 trees should not make much difference in execution time. It is no linear relationship...)
+Execution time is (affine) linear in the number of trees, cf .png
--- code/basecases/speed/speed.R
+++ code/basecases/speed/speed.R
@@ -41,13 +41,40 @@ mb <- microbenchmark(predict(rg,data=df[val,1:10])
                , predict(sf5,data=df[val,1:10])
                )
 
-autoplot(mb)
+#autoplot(mb)
+boxplot(mb, main='why the log on the y-axis?')
 p<-print(mb)
 # par(mfrow=c(1,1))
-plot(x=1:6, y=p$mean, main='mean execution times when halving num.trees', ylab='time in ms', xlab='halving num.trees')
+plot(x=1:6, y=p$mean, main='mean execution times when halving num.trees', ylab='time in ms', xlab='halving num.trees starting with full forest')
 
-p$mean[6] / p$mean[1]
+p$mean[3] / p$mean[1]
+p$mean[4] / p$mean[1]
 
+###########################################################################################
+#### full forest and 10% sub-forest #######################################################
+###########################################################################################
+
+sf1<-subforest(docN[[1]]$ranger$forest,1:50)
+sf2<-subforest(docN[[2]]$ranger, 1:50)
+sf3<-subforest(docN[[3]]$ranger, 1:50)
+
+mb <- microbenchmark(predict(docN[[1]]$ranger,data=df[val,1:10]) 
+                     , predict(sf1,data=df[val,1:10])
+                     , predict(docN[[2]]$ranger,data=df[val,1:10]) 
+                     , predict(sf2,data=df[val,1:10])
+                     , predict(docN[[3]]$ranger,data=df[val,1:10]) 
+                     , predict(sf3,data=df[val,1:10])
+)
+
+#autoplot(mb)
+boxplot(mb, main='full forest and 10% sub-forest')
+p<-print(mb)
+# par(mfrow=c(1,1))
+plot(x=1:6, y=p$mean, main='full forest and 10% sub-forest', ylab='time in ms', xlab='alternating full forest and 10% sub-forest')
+
+p$mean[2] / p$mean[1]
+p$mean[4] / p$mean[3]
+p$mean[6] / p$mean[5]
 
 ##########################################
 #### different scenario: with forests of 50 trees (10% of full forest) how much variation is there (depending on the trees?)
@@ -61,7 +88,6 @@ sf4<-subforest(rg$forest,151:200)
 sf5<-subforest(rg$forest,201:250)
 sf6<-subforest(rg$forest,251:300)
 
-
 mb <- microbenchmark(predict(sf1,data=df[val,1:10])
                      , predict(sf2,data=df[val,1:10])
                      , predict(sf3,data=df[val,1:10])
@@ -73,9 +99,93 @@ mb <- microbenchmark(predict(sf1,data=df[val,1:10])
 #autoplot(mb)
 p<-print(mb)
 # par(mfrow=c(1,1))
-plot(x=1:6, y=p$mean, main='mean execution times for 5 trees', ylab='time in ms', xlab='samples of 50')
+plot(x=1:6, y=p$mean, main='mean execution times for 50 trees', ylab='time in ms', xlab='samples of 50')
 
-max(p$mean) / min(p$mean)
+#######################################################################
+#### forests of 1 and 2 trees, what is the minimum execution time? ####
+#######################################################################
 
-mean(p$mean) / 6.77
+
+sf1<-subforest(rg$forest,1)
+sf2<-subforest(rg$forest,2)
+sf3<-subforest(rg$forest,3)
+sf4<-subforest(rg$forest,4:5)
+sf5<-subforest(rg$forest,6:7)
+sf6<-subforest(rg$forest,8:9)
+
+mb <- microbenchmark(t1a=predict(sf1,data=df[val,1:10])
+                     , t1b=predict(sf2,data=df[val,1:10])
+                     , t1c=predict(sf3,data=df[val,1:10])
+                     , t2a=predict(sf4,data=df[val,1:10])
+                     , t2b=predict(sf5,data=df[val,1:10])
+                     , t2c=predict(sf6,data=df[val,1:10])
+)
+
+#autoplot(mb)
+p<-print(mb)
+# par(mfrow=c(1,1))
+plot(x=1:6, y=p$mean, main='mean execution times for minimal forest\n(1 and 2 trees)', ylab='time in ms', xlab='minimal forest', xaxt='no')
+axis(side = 1
+     , at = 1:6
+     , labels = c(1,1,1,2,2,2)) # rep(1:2,3) 
+
+##################
+#### all in 1 ####
+##################
+
+
+mb <- microbenchmark(ff=predict(rg,data=df[val,1:10])
+                     , t250=predict(subforest(rg,1:250),data=df[val,1:10])
+                     , t125=predict(subforest(rg,1:125),data=df[val,1:10])
+                     , t62=predict(subforest(rg,1:62),data=df[val,1:10])
+                     , t31=predict(subforest(rg,1:31),data=df[val,1:10])
+                     , t16=predict(subforest(rg,1:16),data=df[val,1:10])
+                     , t2=predict(subforest(rg,1:2),data=df[val,1:10])
+                     , t1=predict(subforest(rg,1),data=df[val,1:10])
+                     , times = 1000L
+)
+
+#autoplot(mb)
+p<-print(mb)
+# par(mfrow=c(1,1))
+plot(x=c(500,250,125,62,31,16,2,1), y=p$mean, main='mean execution times', ylab='time in ms', xlab='number of trees', xaxt='no')
+axis(side = 1
+     , at = c(500,250,125,62,31,16,2,1)
+     , labels = c(500,250,125,62,31,16,2,1))
+
+boxplot(mb, log = T)
+
+##################
+#### all in 1 ####
+##################
+
+
+mb <- microbenchmark(predict(rg,data=df[val,1:10])
+                     , predict(subforest(rg,1:450),data=df[val,1:10])
+                     , predict(subforest(rg,1:400),data=df[val,1:10])
+                     , predict(subforest(rg,1:350),data=df[val,1:10])
+                     , predict(subforest(rg,1:300),data=df[val,1:10])
+                     , predict(subforest(rg,1:250),data=df[val,1:10])
+                     , predict(subforest(rg,1:200),data=df[val,1:10])
+                     , predict(subforest(rg,1:150),data=df[val,1:10])
+                     , predict(subforest(rg,1:100),data=df[val,1:10])
+                     , predict(subforest(rg,1:50),data=df[val,1:10])
+                     , predict(subforest(rg,1:30),data=df[val,1:10])
+                     , predict(subforest(rg,1:10),data=df[val,1:10])
+                     , predict(subforest(rg,1:1),data=df[val,1:10])
+                     , times = 1000L
+)
+
+#autoplot(mb)
+p<-print(mb)
+# par(mfrow=c(1,1))
+xgrid = c(seq(from=500, to=50 , length.out=10),30,10,1)
+plot(x=xgrid
+     , y=p$median
+     , main='median execution times'
+     , ylab='time in ms'
+     , xlab='number of trees'
+)
+boxplot(mb, log = T, xaxt='n')
+axis(1,at=xgrid,labels=xgrid)
 
