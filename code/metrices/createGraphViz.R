16ed27b1834e3bac468d7e8aeb801098920ee3b4
silke <s81320@beuth-hochschule.de>
2021-07-06 12:32:17 +0200

    from the ranger treeInfo create code for a vsualization with graphViz

--- code/metrices/createGraphViz.R
+++ code/metrices/createGraphViz.R
@@ -1,8 +1,6 @@
 # for a full binary tree with given split variables 
 # create the graphViz code to visualize the tree
 
-# information on classification in terminal nodes missing
-
 rm(list=ls())
 
 library(dplyr)
@@ -14,33 +12,30 @@ source('code/source/distance-matrices-03-scaled01.R')
 load('code/doc-500trees-10rep-5maxDepth.rda') # adds df (cleveland data set) and docN (10 times: datasplit, RF, distance matrices)
 
 rg <- docN[[1]]$ranger
-#sf <- subforest(rg$forest, c(1,2,3))
-t1 <- rt2fbt(rg$forest, trindx=3)
-t1
+ti<-treeInfo(rg,1)
 
 arrow <- function(a,b) paste("\"",a,"\"->\"",b,"\"",sep='')
-#node  <- function(n,l,c) paste("\"",n,"\" [label=\"", l ,"\" color= ", c , "]", sep='')
-node  <- function(n,l) paste("\"",n,"\" [label=\"", l ,"\" ]", sep='')
+node  <- function(n,l,c) paste("\"",n,"\" [label=\"", l ,"\" color= ", c , "]", sep='')
 
-lines1<- rep("#",62)
-for(i in 1:31){
-  if(t1[i]!=0){
-    if(2*i<32 && t1[2*i]!=0) lines1[i] <- arrow(i,2*i)
+
+triViz <- function(tri, filename="tree.gv"){
+  lines1 <- rep("#", nrow(ti))
+  lines2 <- rep('#', 3*table(ti$terminal)["TRUE"] )
+
+  ct<-0
+
+  for(i in 1:nrow(ti)){
+    if(ti[i,"terminal"]){
+      lines1[i]<-node(ti[i,"nodeID"],ti[i,"prediction"], ifelse(ti[i,"prediction"]=='Yes','red','green'))
+    }else{
+      lines2[ct+1] <- node(ti[i,"nodeID"],ti[i,"splitvarID"],'black')
+      lines2[ct+2] <- arrow(ti[i,"nodeID"], ti[i,"leftChild"])
+      lines2[ct+3] <- arrow(ti[i,"nodeID"], ti[i,"rightChild"])
+      ct <- ct+3
+    }
   }
-}
-for(i in 1:31){
-  if(t1[i]!=0){
-    if(2*i+1<32 && t1[2*i+1]!=0) lines1[31+i] <- arrow(i,2*i+1)
-  }
-}
 
-lines2 <- rep("#",31)
-for(i in 1:31){
-  if(t1[i]!=0){
-    lines2[i]<- node(i,t1[i]-1)}
+  fileConn<-file(filename)
+  writeLines(c("digraph fbt {", "forcelabels=true ;" ,lines2, lines1,"}"), con=fileConn)
+  close(fileConn)
 }
-#lines2
-
-fileConn<-file("a_tree.gv")
-writeLines(c("digraph fbt {", "forcelabels=true ;" ,lines2, lines1,"}"), con=fileConn)
-close(fileConn)
