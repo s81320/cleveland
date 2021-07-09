3a01b1110e36a0a04a9596f1514b88b660def536
silke <s81320@beuth-hochschule.de>
2021-07-09 22:43:47 +0200

    complete overhaul

--- code/basecases/rs/dataEval-baseCase-rs-01.R
+++ code/basecases/rs/dataEval-baseCase-rs-01.R
@@ -1,19 +1,27 @@
+# 8. July 2021
+
+# base case evaluation for the previously generated 10 data splits 
+# with a new ranger random forest grown on each training set
+
+# random sampling subforests in different sizes and 
+# looking at their accuracy ratios (acc of sampled subforest / accuracy of full forest)
+
 rm(list=ls())
-# setwd("~/Documents/ds/sem4/thesis/trees-02-ranger/04-chipman")
 
 library(dplyr)
 library(ranger)
 library(ggplot2)
 
-# source('code/source/distance-matrices-02.R') # my own code
+# my own code
 source('code/source/subforest.R') # constructors for a sub-forest (class ranger.forest) and its hull (class ranger)
-source('code/source/chipman.R')
 source('code/source/helper-functions.R')
 
 # load df (with factors) 
 # load docN with N repetitions for 
 ## data splits and the ranger random forest build on the current train set
-load('code/doc-500trees-10rep-5maxDepth-keepInbag.rda') # adds df (cleveland data set) and docN (10 times: datasplit, RF, distance matrices)
+# load acc.oob - which we do not need for now
+load('data/doc-500trees-10rep-5maxDepth-keepInbag.rda') # adds df (cleveland data set) and docN (10 times: datasplit, RF, distance matrices)
+rm(acc.oob)
 N <- length(docN)
 
 # accuracy of the full forest on the validation set
@@ -21,65 +29,118 @@ aff<-c(rep(0,N))
 par(mfrow=c(1,1))
 for(i in 1:N) aff[i]<-docN[[i]]$accuracy[1]
 boxplot(aff, 
-        main=paste('accuracy full forest, ',N,'repetitons\n(mean:', mean(aff) %>% round(3) , ', sd:', sd(aff) %>% round(3),')'))
+        main=paste('accuracy full forest,',N,'repetitons\n(median:', median(aff) %>% round(3) , ', sd:', sd(aff) %>% round(3),')'))
 summary(aff)
 # focus on variation , measured by standard deviation
-mean(aff) ; sd(aff)
+median(aff) ; mean(aff) ; sd(aff)
 
 ####################################
 #### baseline : random sampling ####
 ####################################
 
-cluster.k<-c(3,5,7,11,13)
-#cluster.k<-c(10,20,30,40,50)
+size.sf<-c(3,5,7,11,13)
+#size.sf<-c(10,20,30,40,50)
 
 rs<-data.frame()
 for(i in 1:N){
   forest <- docN[[i]]$ranger$forest
   val<-docN[[i]]$val
-  accff <- docN[[i]]$accuracy[[1]]
-  for(k in cluster.k){
+  accff <- docN[[i]]$accuracy[['val']]
+  new.row<-c(i,500,round(accff,5),1)
+  rs<-rbind(rs,new.row)
+  for(k in size.sf){
     # mean accuracy of randomly sampled sub-forests
-    for(j in 1:100){
+    for(j in 1:10){
+      acc.sf<- apsf(forest
+                    , sample(1:forest$num.trees, k, replace = FALSE)
+                    , df[val,])
+      
       new.row<- c( i
                    , k
-                   , ( apsf(forest
-                            , sample(1:forest$num.trees, k, replace = FALSE)
-                            , df[val,]
-                   ) / accff ) %>% round(4)
+                   , acc.sf %>% round(5)
+                   , ( acc.sf / accff ) %>% round(5)
       )
       rs<-rbind(rs,new.row)
     }    
   }
 }
-names(rs)<-c('forest','num.trees','accRatio')
+names(rs)<-c('forest','size','acc','acc.ratio')
 
-# rs$num.trees<-as.factor(rs$num.trees)
+##################################
+#### evaluate and save as rda ####
+##################################
 
-stats<-rs %>% group_by(num.trees) %>% summarise(m=mean(accRatio), s=sd(accRatio)) %>% data.frame()
-names(stats)<- c('number of trees','mean accuracy ratio','sd accuracy ratio')
+stats<-rs %>% group_by(size) %>% summarise(mean(acc.ratio)
+                                              , sd(acc.ratio)
+                                              , mean(acc)
+                                              , sd(acc)) %>% data.frame()
+names(stats)<- c('size','mean.acc.ratio','sd.acc.ratio','mean.acc','sd.acc')
 
 baseCase<- list(rs=rs , stats=stats)
+#stats
 rm(rs,stats)
 
-ggplot(baseCase$rs, aes(x=as.factor(num.trees), y=accRatio)) + 
+save(baseCase , file='data/baseCase-rs-500trees.rda')
+#load('data/baseCase-rs-500trees.rda')
+
+###################
+#### visualize ####
+###################
+
+# accuracy ratios
+ggplot(baseCase$rs, aes(x=as.factor(size), y=acc.ratio)) + 
   geom_boxplot() +
   labs(title = 'accuracy ratios for randomly sampled sub-forests')
 
+# accuracies , not ratios
+ggplot(baseCase$rs, aes(x=as.factor(size), y=acc)) + 
+  geom_boxplot() +
+  labs(title = 'accuracies of randomly sampled sub-forests and full forest\n(10 rep each)')
+
 # as the number of trees in the sub-forest grows
 # the mean accuracy ratio goes up and standard deviation goes down
-plot(baseCase$stats[['mean accuracy ratio']]~baseCase$stats[['number of trees']]
+plot(mean.acc.ratio~size
+     , data = baseCase$stats[baseCase$stats$size<500,]
      , main='randomly sampled subforest'
-     , xlab='number of trees'
+     , xlab='size of sub-forest (=nr trees sampled)'
      , ylab='accuracy ratio'
      , type='b')
-plot(baseCase$stats[['sd accuracy ratio']]~baseCase$stats[['number of trees']]
+# numeric values just plotted
+baseCase$stats[['mean.acc.ratio']] # sizes in size.sf , and for the full forest
+
+plot(sd.acc.ratio~size.sf 
+     , data=baseCase$stats[baseCase$stats$size<500,]
      , main='randomly sampled subforest'
      , xlab='number of trees'
      , ylab='standard deviation'
      , type='b')
 
-baseCase$stats[['mean accuracy ratio']]
+###############
+#### model ####
+###############
 
-save(baseCase , file='baseCase-rs-500trees.rda')
-# load('baseCase500trees.rda')
+# don't do this:'
+summary(lm(acc.ratio~0+size , data=baseCase$rs[baseCase$rs$size<500,]))
+#plot(lm(acc.ratio~0+size , data=baseCase$rs[baseCase$rs$size<500,]))
+# an acuracy ratio that goes through the origin is not a good model
+# residuals vs fitted and normal Q-Q look bad
+
+# this doesn't look good either
+# but a model with an intercept close to 1 makes sense
+lm1 <- lm(acc.ratio~size , data=baseCase$rs[baseCase$rs$size<500,])
+summary(lm1)
+# R sqrd almost 0, which is bad
+plot(lm1)
+# residuals and normal QQ look ok
+
+# same for the accuracy (not acc ratio)
+# intercept is close to the mean accuracy of the full forest 0.767, but smaller
+lm2 <- lm(acc~size , data=baseCase$rs[baseCase$rs$size<500,])
+summary(lm2)
+# Intercept and size highly significant, sensible estimates
+# Residual std error at 5% , ok
+# R sqrd almost 0, not ok
+#plot(lm2)
+
+# we will try to get more information, mode predictors to improve the linear model.
+# This will be in dataEval-baseCase-rs-02.R
--- code/basecases/rs/dataEval-baseCase-rs-02.R
+++ code/basecases/rs/dataEval-baseCase-rs-02.R
@@ -1,83 +1,230 @@
 rm(list=ls())
-# setwd("~/Documents/ds/sem4/thesis/trees-02-ranger/04-chipman")
 
 library(dplyr)
 library(ranger)
-library(ggplot2)
-#install.packages('abind')
-library(abind)
 
-# source('code/source/distance-matrices-02.R') # my own code
 source('code/source/subforest.R') # constructors for a sub-forest (class ranger.forest) and its hull (class ranger)
-source('code/source/chipman.R')
 source('code/source/helper-functions.R')
 
 # load df (with factors) 
 # load docN with N repetitions for 
 ## data splits and the ranger random forest build on the current train set
 # load ibc (in bag count) , acc.oob (accuracy on oob training data for each tree) 
-load('code/doc-500trees-10rep-5maxDepth-keepInbag.rda') 
+load('data/doc-500trees-10rep-5maxDepth-keepInbag.rda') 
 
 N <- length(docN)
 
-####################################
-#### baseline : random sampling ####
-####################################
+########################################################
+#### baseline : random sampling ########################
+#### new: collect info on tree distance and oob acc ####
+########################################################
 
-cluster.k<-c(3,5,7,11,13)
-#cluster.k<-c(10,20,30,40,50)
+size.sf<-c(3,5,7,11,13)
+#size.sf<-c(10,20,30,40,50)
 
-rs<-data.frame(matrix(0,nrow=length(cluster.k)*N*10,ncol=6))
+nRS<-100 # number of random samples
+rs<-data.frame(matrix(0,nrow=length(size.sf)*N*nRS+N,ncol=7)) 
+# *nRS because we have nRS different random samples per full forest and per size.sf
+# cf for(j in 1:nRS){ ...
+# +N do also document the accuracies of the full forest, and we have N of them
+dim(rs)
+names(rs)<-c('forest','size','sampled.trees','acc', 'acc.ratio','mean.oob.acc','mean.d1.dissim')
+
 ct<-1 # index counter
 
 for(i in 1:N){
   forest <- docN[[i]]$ranger$forest
   val<-docN[[i]]$val
-  accff <- docN[[i]]$accuracy[[1]]
-  dm<-docN[[i]]$distMatrices$d0
-  preds<-predict(forest, data = df[val,], predict.all = TRUE)
-  for(k in cluster.k){
+  accff <- docN[[i]]$accuracy[['val']]
+  
+  dm<-docN[[i]]$distMatrices$d1
+  # preds<-predict(forest, data = df[val,], predict.all = TRUE) # don't need this ?? !!
+  
+  rs[ct,]<-data.frame(i
+              , 500
+              , '1:500'
+              , round(accff,5)
+              , 1
+              , mean(acc.oob[i,]) %>% round(5) 
+              , mean(dm[upper.tri(dm)])  %>% round(5)
+              )
+  ct<-ct+1
+
+  for(k in size.sf){
     # mean accuracy of randomly sampled sub-forests
-    for(j in 1:100){
+    for(j in 1:nRS){
       trindcs<-sample(1:forest$num.trees, k, replace = FALSE)
-      
-      rs[ct,]<- c(i # which data split, which full forest
-                  ,k # number of clusters
-                  , paste(trindcs[order(trindcs)],collapse=',') # add this to check if something is strange
-                  , ( apsf(forest
-                              , trindcs
-                              , df[val,] ) / accff ) %>% round(3) # accuracy ratio on validation set
-                  , mean(acc.oob[i,trindcs]) %>% round(3) # accuracy on oob training data
-                  , (sum(dm[trindcs,trindcs])/(k*(k-1)))  %>% round(3) # mean distance of sampled trees , not using mean
+      accsf<-apsf(forest, trindcs, df[val,]) 
+
+      rs[ct,]<- data.frame('forest'=i # which data split, which full forest
+                  , 'size'=k # number of clusters, size of sub-forest
+                  , 'sampled.trees'=paste(trindcs[order(trindcs)],collapse=',') # add this to check if something is strange
+                  , 'acc'=accsf
+                  , 'acc.ratio'=(accsf / accff) %>% round(5) # accuracy ratio on validation set
+                  , 'mean.oob.acc'=mean(acc.oob[i,trindcs]) %>% round(5) # accuracy on oob training data
+                  , 'mean.d1.dissim'=(sum(dm[trindcs,trindcs])/(k*(k-1)))  %>% round(5) # mean distance of sampled trees , not using mean
+                  # , 'mean.d1.dissim'=mean(dm[trindcs,trindcs][upper.tri(dm[trindcs,trindcs])]  %>% round(5) # mean distance of sampled trees , not using mean
                   )
       ct<-ct+1
     }    
   }
 }
-names(rs)<-c('forest','num.trees','sampled trees','accRatio','mean tree oob acc','mean tree dissimilarity')
-# if we didn't have the string for the sampled trees, all variables would be numeric 
-# and the following would be unneccessary
-rs$forest<-as.numeric(rs$forest)
-rs$num.trees<-as.numeric(rs$num.trees)
-rs$accRatio<-as.numeric(rs$accRatio)
-rs[,'mean tree oob acc'] <- as.numeric(rs[,'mean tree oob acc']) 
-rs[,"mean tree dissimilarity"] <- as.numeric(rs[,"mean tree dissimilarity"])
 
 
-lm1<-lm(accRatio~0+. , data=rs[,c(2,4:6)])
+##########################################
+#### save documented / generated data ####
+##########################################
+
+stats<-rs %>% 
+  group_by(size) %>% 
+  summarise('mean.acc'=mean(acc)
+            , 'sd.acc'=sd(acc)
+            , 'mean.acc.ratio'=mean(acc.ratio)
+            , 'sd.acc.ratio'=sd(acc.ratio)
+            , 'mean.mean.oob.acc'= mean(mean.oob.acc)
+            , 'sd.mean.oob.acc'=sd(mean.oob.acc)
+            , 'mean.mean.d1.dissim'=mean(mean.d1.dissim)
+            , 'sd.mean.d1.dissim'=sd(mean.d1.dissim)
+            ) %>% 
+  data.frame()
+
+stats
+
+#baseCase02<- list(info='sunny today 8-July-2021 :-)' , rs=rs , stats=stats)
+
+#file='data/baseCase02-rs-500trees.rda'
+#save(baseCase02 , file=file)
+#load(file)
+#rs<-baseCase02$rs
+#stats<-baseCase02$stats
+
+###################
+#### visualize ####
+###################
+
+ggplot(rs, aes(x=as.factor(size), y=acc.ratio)) + 
+  geom_boxplot() +
+  labs(title = 'accuracy ratios for randomly sampled sub-forests')
+
+ggplot(rs, aes(x=as.factor(size), y=acc)) + 
+  geom_boxplot() +
+  labs(title = 'accuracies for randomly sampled sub-forests')
+
+# as the number of trees in the sub-forest grows
+# the mean accuracy ratio goes up and standard deviation goes down
+plot(mean.acc~size
+     , data = stats[stats$size<500,]
+     , main='randomly sampled subforest\n(accuracy)'
+     , xlab='forest size'
+     , ylab='mean accuracy'
+     , type='b')
+# numeric values just plotted
+stats$mean.acc
+
+plot(sd.acc~size
+     , data = stats[stats$size<500,]
+     , main='randomly sampled subforest\n(accuracy)'
+     , xlab='forest size'
+     , ylab='standard deviation for accuracy'
+     , type='b')
+
+plot(mean.acc.ratio~size
+     , data = stats[stats$size<500,]
+     , main='randomly sampled subforest\n(accuracy ratios)'
+     , xlab='frest size'
+     , ylab='mean accuracy ratio'
+     , type='b')
+# numeric values just plotted
+stats$mean.acc.ratio
+
+plot(sd.acc.ratio~size
+     , data = stats[stats$size<500,]
+     , main='randomly sampled subforest\n(accuracy ratios)'
+     , xlab='forest size'
+     , ylab='standard deviation for accuracy ratio'
+     , type='b')
+
+##########################################
+#### linear models for interpretation ####
+##########################################
+
+lm1<-lm(acc.ratio~0+size+mean.oob.acc+mean.d1.dissim , data=rs[rs$size<500,])
 summary(lm1)
+# all predictors highly significant, R-sqrd almost 1 -> all variance explained
 # this is good!!
 
-# does this tell us that the mean tree acc is most important?
-lm2<-lm(rs$accRatio~0+rs[,'mean tree oob acc'])
+# now with an intercept / a constant
+lm2<-lm(acc.ratio~size+mean.oob.acc+mean.d1.dissim , data=rs[rs$size<500,])
 summary(lm2)
-# I should do an ANOVA thing to compare nested models...
+lm3<-lm(acc.ratio~mean.d1.dissim , data=rs[rs$size<500,])
+summary(lm3)
+# this is so poor :-(
+# R sqrd almost 0 , mean oob acc and mean d1 dissim not significant
+
+#plot(lm1)
+
+# this is what we want in both models: lm1 and lm2
+predict(lm1,
+        newdata=data.frame('size'=11 , 'mean.oob.acc'=0.7 , 'mean.d1.dissim'=0.3))
+
+# why is this so bad ??
+plot(acc.ratio~mean.oob.acc 
+     # , data=rs[(rs$size.sf==13 & rs$mean.d1.dissim>0.3),]
+     , data=rs[rs$size<500,]
+     , asp=1 
+     , xlab='mean individual oob accuracy'
+     , ylab='ensemble accuracy ratio (on val set)'
+     , main='explain forest performance\nby mean of tree performance')
+
+# because the res std error is so large? 6.7% can make a huge difference : 0.94 or 1.06 accuracy ratio?
+
+library(ggplot2)
+#install.packages('ggExtra')
+library(ggExtra) # for the marginal plot
+#install.packages('hrbrthemes')
+library(hrbrthemes) # theme_ipsum
+
+p1 <- ggplot(rs[rs$size<500,], aes(x=mean.oob.acc, y=acc.ratio, colour=size)) +
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="blue", se=TRUE) +
+  theme(legend.position="bottom")
+p2 <- ggMarginal(p1, type="density")
+p2
+rm(p1,p2)
+
+# maybe this is not so bad?
+# there is a clear upward slope with a tight conf region around it
+# increasing nRS makes it really tight
+# we are dealing with small change here, an increased accuracy ratio of 3% is quite a lot
+
+# -> that is why the residual std error of around 6% bothers me ... ups, negative vibes again
+
+#######################
+#### dissimilarity ####
+#######################
+
+p3 <- ggplot(rs[rs$size<500,], aes(x=mean.d1.dissim, y=acc.ratio, colour=size)) +
+  geom_point() +
+  geom_smooth(method=lm , color="red", fill="blue", se=TRUE) +
+  theme(legend.position="bottom")
+ 
+p4 <- ggMarginal(p3, type="density")
+
+p4
+rm(p3,p4)
+
+# the line is less steep , as expected from the linear model BUT
+# for rather dissimilar trees (high values of mean.d1.dissim) most acc.ratios are high. There is only one exception
+# as would be expected, small forests have a higher spread over the dissimilarity values
+# repeating this (different sampling) it does not hold: rather dissimilar trees most acc.ratios are high!
 
 ####################################################
 #### remains from developing the myAcc function ####
 ####################################################
 #### will soon go ... ##############################
 ####################################################
+
+library(abind)
 
 # example: turning a list with elements of the same length into a matrix
 abind(docN[[1]]$ranger$inbag.counts, along=2) %>% dim
